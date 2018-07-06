da3631f687d1ec72a3cc2081fbdec597310bb3f6
Jörg Siebahn <joerg@siebahn.de>
2018-07-06 07:16:47 +0200

    Replace pegdown in favour of flexmark

--- markdown/src/test/java/com/github/jsiebahn/various/tests/markdown/MarkdownTest.java
+++ markdown/src/test/java/com/github/jsiebahn/various/tests/markdown/MarkdownTest.java
@@ -1,14 +1,29 @@
 package com.github.jsiebahn.various.tests.markdown;
 
+import com.vladsch.flexmark.ext.abbreviation.AbbreviationExtension;
+import com.vladsch.flexmark.ext.definition.DefinitionExtension;
+import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
+import com.vladsch.flexmark.ext.tables.TablesExtension;
+import com.vladsch.flexmark.ext.typographic.TypographicExtension;
+import com.vladsch.flexmark.ext.yaml.front.matter.YamlFrontMatterExtension;
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.profiles.pegdown.Extensions;
+import com.vladsch.flexmark.profiles.pegdown.PegdownOptionsAdapter;
+import com.vladsch.flexmark.util.options.DataHolder;
+import com.vladsch.flexmark.util.options.MutableDataSet;
 import org.apache.commons.io.IOUtils;
-import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.Test;
-import org.pegdown.Extensions;
-import org.pegdown.PegDownProcessor;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.fail;
 
 /**
@@ -17,60 +32,65 @@ import static org.junit.Assert.fail;
  * @author  jsiebahn
  * @since 25.09.14 07:09
  */
+@RunWith(Parameterized.class)
 public class MarkdownTest {
 
-    @Test
-    public void testFindContentAfterMetadata() {
+    private Parser parser;
+    private HtmlRenderer htmlRenderer;
 
-        PegDownProcessor processor = new PegDownProcessor(Extensions.ALL + Extensions.SUPPRESS_ALL_HTML);
-        String actual = processor.markdownToHtml(
-                "*[layout]: post\n"
-                + "*[published-on]: 1 January 2000\n"
-                + "*[title]: Blogging Like a Boss\n"
-                + "\n"
-                + "Content goes here.");
-        assertEquals("<p>Content goes here.</p>", actual);
-    }
+    private String testFilename;
 
-    @Test
-    @Ignore("Yaml metadata not working with pegdown.")
-    public void testFindContentAfterYamlMetadata() {
-
-        PegDownProcessor processor = new PegDownProcessor(Extensions.ALL + Extensions.SUPPRESS_ALL_HTML);
-        String actual = processor.markdownToHtml("---\n"
-                + "layout: post\n"
-                + "published-on: 1 January 2000\n"
-                + "title: Blogging Like a Boss\n"
-                + "---\n"
-                + "\n"
-                + "Content goes here.");
-        assertEquals("<p>Content goes here.</p>", actual);
-    }
-
-    @Test
-    public void testCreateBlockHtml() {
-
-        PegDownProcessor processor = new PegDownProcessor(Extensions.ALL + Extensions.SUPPRESS_ALL_HTML);
-
-        testAll(processor,
-                "HelloWorld",
-                "UnorderedList",
-                "OneParagraph",
-                "TwoParagraphs",
-                "InlineBold",
-                "Ellipses",
-                "SimpleLink",
-                "DefinitionList",
-                "Quotes"
+    @Before
+    public void setUp() {
+        DataHolder OPTIONS = PegdownOptionsAdapter.flexmarkOptions(
+                Extensions.ALL
         );
+        MutableDataSet options = new MutableDataSet();
+        // uncomment to set optional extensions
+        options.set(Parser.EXTENSIONS, Arrays.asList(
+                TablesExtension.create(),
+                StrikethroughExtension.create(),
+                DefinitionExtension.create(),
+                TypographicExtension.create(),
+                YamlFrontMatterExtension.create(),
+                AbbreviationExtension.create()
+                )
+        );
+        options.set(HtmlRenderer.GENERATE_HEADER_ID, true);
+        options.set(HtmlRenderer.HEADER_ID_GENERATOR_RESOLVE_DUPES, true);
+        options.set(HtmlRenderer.INDENT_SIZE, 2);
 
+        parser = Parser.builder(options).build();
+        htmlRenderer = HtmlRenderer.builder(options).build();
     }
 
-    private void testAll(PegDownProcessor processor, String... filenames) {
-        for (String testFile : filenames) {
-            assertEquals("Parsing failed for '" + testFile + "'",
-                    normalize(getHtml(testFile)), normalize(processor.markdownToHtml(getMd(testFile))));
-        }
+    @Parameters(name = "{0}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(
+                new Object[] {"HelloWorld"},
+                new Object[] {"UnorderedList"},
+                new Object[] {"OneParagraph"},
+                new Object[] {"TwoParagraphs"},
+                new Object[] {"InlineBold"},
+                new Object[] {"Ellipses"},
+                new Object[] {"SimpleLink"},
+                new Object[] {"DefinitionList"},
+                new Object[] {"Quotes"},
+                new Object[] {"MetadataAbbreviations"},
+                // new Object[] {"MetadataPandoc"}, Seems to be not supported
+                // new Object[] {"MetadataMultimarkdown"}, // there seems to be a general profile
+                new Object[] {"MetadataYaml"}
+                );
+    }
+
+    public MarkdownTest(String testFilename) {
+        this.testFilename = testFilename;
+    }
+
+    @Test
+    public void test() {
+        assertThat(normalize(htmlRenderer.render(parser.parse(getMd(testFilename)))))
+                .isEqualTo(normalize(getHtml(testFilename)));
     }
 
     private String getMd(String filenameWithoutExtension) {
@@ -91,6 +111,6 @@ public class MarkdownTest {
     }
 
     private String normalize(String s) {
-        return s.replaceAll("(\r\n|\r|\n)", "\n");
+        return s.replaceAll("(\r\n|\r|\n)", "\n").trim();
     }
 }
---
+++ markdown/src/test/resources/com/github/jsiebahn/various/tests/markdown/MetadataPandoc.md
@@ -0,0 +1,5 @@
+% layout post
+% published-on 1 January 2000
+% title Blogging Like a Boss
+            
+Content goes here.
